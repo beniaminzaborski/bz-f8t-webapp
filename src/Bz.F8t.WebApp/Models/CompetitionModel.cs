5cf2eda407032b044dec7d663a0a77f02d641b5a
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2024-01-24 18:27:20 +0100

    Competition Form validation

--- src/Bz.F8t.WebApp/Models/CompetitionModel.cs
+++ src/Bz.F8t.WebApp/Models/CompetitionModel.cs
@@ -1,17 +1,37 @@
 ﻿using Bz.F8t.WebApp.ApiClients.Dtos;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bz.F8t.WebApp.Models;
 
 public class CompetitionModel
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
     public string City { get; set; }
+
+    [Range(-90.0000000, 90.0000000)]
     public decimal Latitude { get; set; }
+
+    [Range(-180.0000000, 180.0000000)]
     public decimal Longitute { get; set; }
+
     public DateTime StartDate { get; set; } = DateTime.UtcNow;
+
     public DateTime StartTime { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    [Range(0, 1000000)]
     public decimal Distance { get; set; }
+
+    [Required(ErrorMessage = "The Distance Unit field is required.")]
+    //[EnumDataType()]
     public string DistanceUnit { get; set; }
+
+    [Required(ErrorMessage = "The Maximum Number Of Competitors field is required.")]
+    [Range(1, int.MaxValue, ErrorMessage = "The field  Maximum Number Of Competitors must be between {1} and {2}.")]
     public int MaxNumberOfCompetitors { get; set; }
 
     public static CompetitionModel FromCompetitionDto(CompetitionDto dto)
